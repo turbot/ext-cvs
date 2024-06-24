c731b3b899a65ba36d9723af1f317022a206e011
David Boeke <david@boeke.com>
2024-06-24 12:33:15 -0400

    add default tags

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
 provider "aws" {
   region = var.aws_region
   profile = var.aws_profile
+
+  default_tags {
+    tags = {
+      applicationname    = "Turbot"
+      costcenter         = "190320"
+      dataclassification = "proprietary"
+      resourceowner      = "Robert Muturi"
+      itpmid             = "78CD82E663CA0E99"
+    }
+  }
 }
